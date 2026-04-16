64e06a5df2d9eba756e81db385263d3175f86263
SniksaX <papoosep1@gmail.com>
2026-04-16 12:39:45 +0200

    [FIX] layout dynamic + env.example

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion des Cadeaux</title>
+    <title>gestion des cadeaux</title>
 </head>
 <body>
     <header>
         <nav>
-            <a href="{{ route('gifts.index') }}">Accueil (Liste des cadeaux)</a>
+            <a href="{{ route('gifts.index') }}">Accueil</a>
         </nav>
         <hr>
     </header>
