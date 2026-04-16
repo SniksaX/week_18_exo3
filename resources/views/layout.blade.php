90873e0854cfdeca8f4fc8a48356474969cef9b5
SniksaX <papoosep1@gmail.com>
2026-04-16 12:38:38 +0200

    [FIX] layout dynamic + env.example

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>gestion des cadeaux</title>
+    <title>Gestion des Cadeaux</title>
 </head>
 <body>
     <header>
         <nav>
-            <a href="{{ route('gifts.index') }}">accueil</a>
+            <a href="{{ route('gifts.index') }}">Accueil (Liste des cadeaux)</a>
         </nav>
         <hr>
     </header>
